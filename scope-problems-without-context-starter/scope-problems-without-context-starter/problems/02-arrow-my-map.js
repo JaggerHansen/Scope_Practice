61492aefe4f8a9a5d93a2b01b6a6bf962f09bf3a
justinguu <justinguuu@gmail.com>
2022-05-19 16:10:02 -0400

    finished#3

--- scope-problems-without-context-starter/scope-problems-without-context-starter/problems/02-arrow-my-map.js
+++ scope-problems-without-context-starter/scope-problems-without-context-starter/problems/02-arrow-my-map.js
@@ -8,6 +8,18 @@ Do not use the built in Array#map - use Array#forEach for iteration.
 
 
 Examples:
+
+***********************************************************************/
+const arrowMyMap = (arr, cb) => {
+  let newArr = []
+    arr.forEach((el) => {
+      newArr.push(cb(el))
+
+    })
+    return newArr
+}
+
+
 let result1 = arrowMyMap([100, 25, 81, 64], Math.sqrt);
 console.log(result1);   // [ 10, 5, 9, 8 ]
 
@@ -16,9 +28,6 @@ const yell = el => el.toUpperCase() + '!'
 let result2 = arrowMyMap(['run', 'Forrest'], yell);
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
-***********************************************************************/
-
-// your code here!
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
